219c23be60f42707d7d2fe194d4a90252e3c178a
NoahStephens <noah@cphandheld.com>
2024-08-18 15:11:08 -0600

    added echos to install script

--- bin/install.sh
+++ bin/install.sh
@@ -22,18 +22,22 @@ else
   exit
 fi
 
+ehco "creating python virtual env venv"
 python3 -m venv venv
 source venv/bin/activate
+echo "installing reqs..."
 pip install -r requirements.txt
 
 # Build system control serice
+echo "building systemd script"
 PYTHONPATH=$PWD $PWD/venv/bin/python sysctl/build.py
 deactivate
 
 # Install Service (Must be sudoer) also systemctl isn't supported by MacOS
 for file in sysctl/*.service; do
-  echo "copying " $file "to /etc/systemd/system"
+  echo "copying " $file " to /etc/systemd/system"
   cp $file /etc/systemd/system/
   systemctl deamon-reload
+  echo "starting service..."
   systemctl start "${file##*/}"
 done
